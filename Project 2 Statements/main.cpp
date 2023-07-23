3b729fb285aa69801c761c8548a6541db23e9a12
CodyBenn <zolnyy@yahoo.com>
2023-07-22 23:05:28 -0500

    Project 2 elaboration

--- Project 2 Statements/main.cpp
+++ Project 2 Statements/main.cpp
@@ -2,6 +2,9 @@
 
 int main(int argc, char **argv)
 {
+    //Statements are the lines of code we write ending with ";".
+    //Below are some examples made by assigning variables and creating a "Function".
+
     int firstNumber = 12;
     int secondNumber = 9;
 
@@ -10,4 +13,6 @@ int main(int argc, char **argv)
     std::cout << "The sum of the two numbers is : " << sum << std::endl;
 
     return 0;
+
+    //To learn more about Functions, check Project 3 Functions.
 }
