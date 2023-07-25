1270bee25b7752d8213dd4691361e3c127fb8289
CodyBenn <zolnyy@yahoo.com>
2023-07-25 11:03:05 -0500

    Prep for Project 5

---
+++ Project 5 Program Execution/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    //Prints Hello World! and other lines to the console.
+
+    std::cout << " Hello World! \n"
+    " I was just created and wish to be free! \n"
+    " At least give me a purpose outside of handling butter! ";
+        return 0;
+
+    //Program ends here.
+    //This is a comment made to waste your time, today I wasted about 2 seconds! :)
+}
