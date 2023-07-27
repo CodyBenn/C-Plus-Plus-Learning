09f297acb4fce9a3e2448556b00f131af250223f
CodyBenn <zolnyy@yahoo.com>
2023-07-26 21:07:33 -0500

    Project 5 Program Execution

--- Project 5 Program Execution/main.cpp
+++ Project 5 Program Execution/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+//This program is used as an example to show the order of operations in a program.
+int add_numbers(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
+    //
+    int a = 10;
+    int b = 5;
+    int c;
+    
+    std::cout << "Statement1" << std::endl;
+    std::cout << "Statement2" << std::endl;
+    c = add_numbers(a, b);
+    std::cout << "Statement3" << std::endl;
+    std::cout << "Statement4" << std::endl;
 
+    return 0;
 }
