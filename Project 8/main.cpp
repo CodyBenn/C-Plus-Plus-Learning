9e7d0f330d7ff93d97f7cc6d34d8fa407b8772a5
CodyBenn <zolnyy@yahoo.com>
2023-07-29 22:17:18 -0500

    Project 8 Switches

--- Project 8/main.cpp
+++ Project 8/main.cpp
@@ -2,6 +2,21 @@
 
 int main()
 {
-    //
-    
+    //Lets learn about switches, "choice"
+    int choice;
+    std::cin >> choice;
+    switch(choice)
+    {
+        case 1:
+            std::cout << "Coffee";
+            break;
+        case 2:
+            std::cout << "Tea";
+            break;
+        case 3 :
+            std::cout << "Soda";
+            break;
+        default:
+            std::cout << "Please pick 1 - 3.";
+    }
 }
