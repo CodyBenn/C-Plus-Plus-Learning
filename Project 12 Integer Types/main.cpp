005b5e98e3f580900e4e6ee4d89f313230077e15
CodyBenn <zolnyy@yahoo.com>
2023-08-03 22:29:54 -0500

    Project 12 Int Types

--- Project 12 Integer Types/main.cpp
+++ Project 12 Integer Types/main.cpp
@@ -11,14 +11,40 @@ int main()
     //Can use expression as initializer
     int domesticated_animals {dog_count + cat_count};
 
-    //int new_number {doesnt_exist};
-    //int narrowing_conversion{2.9};
+    //int narrowing_conversion{2.9}; //Gives compiler Error for result
 
-    std::cout << "Elephant count: " << elephant_count << std::endl;
-    std::cout << "Lion count: " << lion_count << std::endl;
-    std::cout << "Dog count: " << dog_count << std::endl;
-    std::cout << "Cat count: " << cat_count << std::endl;
-    std::cout << "Domesticated animal count: " << domesticated_animals << std::endl;
+    /*
+    std::cout << "Elephant count : " << elephant_count << std::endl;
+    std::cout << "Lion count : " << lion_count << std::endl;
+    std::cout << "Dog count : " << dog_count << std::endl;
+    std::cout << "Cat count : " << cat_count << std::endl;
+    std::cout << "Domesticated animal count : " << domesticated_animals << std::endl;
+    */
+
+    //Functional Initialization
+    int apple_count(5);
+    int orange_count(10);
+    int fruit_count(apple_count + orange_count);
+
+    //Information lost. less safe than braced initializers.
+    int narrowing_conversion_functional (2.9); //Chops off decimal, using whatever number comes first
+
+    /*
+    std::cout << "Apple count : " << apple_count << std::endl;
+    std::cout << "Orange count : " << orange_count << std::endl;
+    std::cout << "Fruit count : " << fruit_count << std::endl;
+    std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl;
+    */
+
+    int bike_count = 2;
+    int truck_count = 7;
+    int vehicle_count = bike_count + truck_count;
+    int narrowing_conversion_assignment = 2.9;
+    
+    std::cout << "Bike count : " << bike_count << std::endl;
+    std::cout << "Truck count : " << truck_count << std::endl;
+    std::cout << "Vehicle count : " << vehicle_count << std::endl;
+    std::cout << "Narrowing conversion : " << narrowing_conversion_assignment << std::endl;
 
     return 0;
 }
