2c9cc44a964c96b55993176821437270bc9b5b86
CodyBenn <zolnyy@yahoo.com>
2023-08-03 22:43:10 -0500

    Project 12 Int Types

--- Project 12 Integer Types/main.cpp
+++ Project 12 Integer Types/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    //Braced Initializers
+    //Braced Initializers ***********
     int elephant_count; //Variable may contain random garbage value.
     int lion_count{};   //Initialize to 0
     int dog_count{10};  //Initialize to 10
@@ -21,7 +21,7 @@ int main()
     std::cout << "Domesticated animal count : " << domesticated_animals << std::endl;
     */
 
-    //Functional Initialization
+    //Functional Initialization ***************
     int apple_count(5);
     int orange_count(10);
     int fruit_count(apple_count + orange_count);
@@ -36,11 +36,12 @@ int main()
     std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl;
     */
 
+    //Assignment Notation ******************
     int bike_count = 2;
     int truck_count = 7;
     int vehicle_count = bike_count + truck_count;
     int narrowing_conversion_assignment = 2.9;
-    
+
     std::cout << "Bike count : " << bike_count << std::endl;
     std::cout << "Truck count : " << truck_count << std::endl;
     std::cout << "Vehicle count : " << vehicle_count << std::endl;
