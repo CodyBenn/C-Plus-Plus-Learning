0a3fb67aa80ccfe996d015954399aa0b3e4a09ea
CodyBenn <zolnyy@yahoo.com>
2023-08-04 21:13:38 -0500

    Project 13 TASK Arrays

--- Project 13 TASK Arrays/main.cpp
+++ Project 13 TASK Arrays/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+    /*
+    Alphabet
+
+    The given code delcares an array that holds the letters of the English alphabet
+    TASK
+    Take a number as input and output the letter from the array that corresponds to that index
+    */
+
 int main()
 {
-    //
-    
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    int number;
+    std::cin >> number;
+    std::cout << letters[number] << std::endl;
+
 }
