1c22cf64c1f5b1b5629e68a2afd12638fd313992
CodyBenn <zolnyy@yahoo.com>
2023-08-05 15:36:03 -0500

    Project 15

--- Project 15 Fractional Numbers/main.cpp
+++ Project 15 Fractional Numbers/main.cpp
@@ -2,6 +2,26 @@
 
 int main()
 {
-    //
-    
+    bool red_light {true};
+    bool green_light {false};
+
+    if (red_light == true)
+    {
+        std::cout << "Stop!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Go through!" << std::endl;
+    }  
+
+    if(green_light);
+    {
+        std::cout << "Light is green!" << std::endl;
+    }
+    else
+    {
+        std::cout << "The light is NOT green" << std::endl;
+    }
+
+    return 0;
 }
