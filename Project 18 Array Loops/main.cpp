15b955639d273d535a01179187a6cf96936b71b3
CodyBenn <zolnyy@yahoo.com>
2023-08-08 21:54:17 -0500

    Project 18 Array Loops

--- Project 18 Array Loops/main.cpp
+++ Project 18 Array Loops/main.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-    //
-    
+    double prices[] = 
+    {
+        6.99, 8.5, 42.9, 105.4, 42, 98.1, 2.39, 1.99, 68.76, 9.99, 33.67, 105, 99, 67, 24.56, 0.2, 59.87, 77.2
+    };
+    double total;
+    for(int x = 0; x < 18; x++)
+    {
+        total += prices[x];
+    }
+    std::cout << total << std::endl;
 }
