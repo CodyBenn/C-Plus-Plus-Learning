3192064f4712fbe810545680393d9c2d81496c8e
CodyBenn <zolnyy@yahoo.com>
2023-08-17 21:33:23 -0500

    Project 26 Loops Again

--- Project 26 Loops Again/main.cpp
+++ Project 26 Loops Again/main.cpp
@@ -4,15 +4,24 @@ int main()
 {
     //For
 
-    int i;
-    for(unsigned int i {}; i < 10; ++i)
+    std::cout << "=== FOR LOOPS ===" << std::endl;
+
+    for(unsigned int i {0}; i < 10000; ++i) //Assigns int = i; runs i until it reaches 10 times; adds +1 to i until finished.
     {
-        std::cout << "Help, I've fallen" << std::endl;
+        std::cout << i << " : Help, I've fallen" << std::endl; //Prints "Help I've fallen" 10 times.
     }
+    std::cout << "Loop is finished" << std::endl;
+
+    //==================================================================================================================
+    std::cout << "==============================" << std::endl;
+    //==================================================================================================================
 
     //Range Based
 
     //While
 
     //Do While
+
+
+    return 0;
 }
