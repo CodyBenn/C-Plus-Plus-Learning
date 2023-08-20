b56245b4d0831ed0b091b479fba80f150beaf2f1
CodyBenn <zolnyy@yahoo.com>
2023-08-19 23:03:53 -0500

    Project 30 Dynamic Memory

--- Project 30 Dynamic Memory/main.cpp
+++ Project 30 Dynamic Memory/main.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
-    //
+    int n;
+    std::cin >> n;
     
+    int* nums = new int [n];
+    for(int i = 0; i < n; i++)
+    {
+        std::cin >> nums[i];
+    }
+
+    int max = nums[0];
+    for(int i = 0; i < n; i++)
+    {
+        if(nums[i] > max)
+            max = nums[i];
+    }
+    std::cout << max << std::endl;
+    
+    return 0;
 }
