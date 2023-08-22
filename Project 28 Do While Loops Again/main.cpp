a568002942b150e0ca874506b756958a361b5e1a
CodyBenn <zolnyy@yahoo.com>
2023-08-21 22:04:52 -0500

    Project 28 Do While Loops Again

--- Project 28 Do While Loops Again/main.cpp
+++ Project 28 Do While Loops Again/main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-    //
+    const int COUNT{10};
+    size_t i{0};
+
+    do
+    {
+        std::cout << i << " : I love C++" << std::endl; //Iterator declaration
+
+        ++i; //Incrementation
+    }
+    while(i < COUNT);
     
+    std::cout << "Loop is finished" << std::endl;
+
+    return 0;
 }
