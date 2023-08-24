dc783428414aeaa8e8ed2a1612d7d94e29f6cc3d
CodyBenn <zolnyy@yahoo.com>
2023-08-23 21:48:14 -0500

    Project 31 Declaring And Using Arrays

--- Project 31 Declaring And Using Arrays/main.cpp
+++ Project 31 Declaring And Using Arrays/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     //Declare an array of ints
 
-    int scores[10]; //Junk data, prints a random number.
+    //int scores[10]; //Junk data, prints a random number.
 
     /*
     //Read data
@@ -43,11 +43,45 @@ int main()
     */
 
     //Declare and initialize at the same time
-
+    /*
     double salaries [5] {12.7, 7.5, 13.2, 8.1, 9.3};
 
     for(size_t i {0}; i < 5; ++i){
         std::cout << "salary [" << i << "] : " << salaries [i] << std::endl;
     }
+    */
+
+    //If you dont initialize all the elements, those you leave out are initialized to 0
+    /*
+    int families [5] {12, 7, 5};
+
+    for(size_t i {0}; i < 5; ++i){
+        std::cout << "families[" << i << "] : " << families[i] << std::endl;
+    } 
+    */
+
+    //Omit the size of the array at declaration
+    /*
+    int class_sizes[] {10, 12, 15, 11, 18, 17, 23, 56};
+
+    //Will print this wit ha range based for loop
+
+    for(auto value : class_sizes){
+        std::cout << "value : " << value << std::endl;
+    }
+    */
+
+    // Read only arrays
+    //const int birds [] {10, 12, 15, 11, 18, 17, 23, 56};
+    //birds[2] = 8;
+
+    int scores[] {2, 5, 8, 2, 5, 6, 9};
+    int sum{0};
+
+    for(int element : scores) {
+        sum += element;
+    }
+    std::cout << "Score sum : " << sum << std::endl;
+
     return 0;
 }
