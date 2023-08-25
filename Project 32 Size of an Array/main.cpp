26f40c9c4c72e434c9f18cf8d5ab1451901c5927
CodyBenn <zolnyy@yahoo.com>
2023-08-24 21:48:23 -0500

    Project 32 Size of an Array

--- Project 32 Size of an Array/main.cpp
+++ Project 32 Size of an Array/main.cpp
@@ -4,11 +4,27 @@ int main()
 {
     int scores [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
+    /*
+    Before C++17 
+    -------------------------------------------------------
+    std::cout << "sizeof(scores) : " << sizeof(scores) << std::endl;
+    std::cout << "sizeof(scores0) : " << sizeof(scores[0]) << std::endl;
 
+    int count {sizeof(scores)/sizeof(scores[0])}
+    */
+   
     int count {std::size(scores)};
 
-
-    for(size_t i = 0; i > count; ++i){
+    /*
+    for(size_t i = {0}; i < count; ++i){
         std::cout << "scores [" << i << "] : " << scores[i] << std::endl;
     }
+    */
+
+    //Range based for loop
+    for ( auto i : scores){
+        std::cout << "value : " << i << std::endl;
+    }
+    
+    return 0;
 }
