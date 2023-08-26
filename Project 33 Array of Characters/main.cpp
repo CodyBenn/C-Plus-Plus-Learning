0717d968bf638cd24b32e75a160e9e676d4c0488
CodyBenn <zolnyy@yahoo.com>
2023-08-25 23:25:58 -0500

    Project 33 Array of Characters

--- Project 33 Array of Characters/main.cpp
+++ Project 33 Array of Characters/main.cpp
@@ -33,9 +33,26 @@ int main()
     //If a character array is null terminated, it is called as C-String
     char message1[] {'H', 'e', 'l', 'l', 'o', '\0'};
     std::cout << "message1 : " << message1 << std::endl;
+    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
 
     char message2[6] {'H', 'e', 'l', 'l', 'o'};
     std::cout << "message2 : " << message2 << std::endl;
+    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
+
+    //This is not a C-String, as there is no Null Character, '\0'
+    char message3[] {'H', 'e', 'l', 'l', 'o'};
+    std::cout << "message3 : " << message3 << std::endl;
+    std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl;
+ 
+    //String Literal, the most commonly used
+    char message4[] {"Hello World, how are you doing!"};
+    std::cout << "message4 : " << message4 << std::endl;
+    std::cout << "sizeof(message4) : " << sizeof(message4) << std::endl;
+
+    //Cant safely print out arrays other than those of characters
+    int numbers[] {1, 2, 3, 4, 5};
+    std::cout << "numbers : " << numbers << std::endl;
+
 
     return 0;
 }
