1a65cc706d9ff91c1242bbfb34ba0d95427af294
CodyBenn <zolnyy@yahoo.com>
2023-08-25 22:54:12 -0500

    Project 33 Array of Characters

--- Project 33 Array of Characters/main.cpp
+++ Project 33 Array of Characters/main.cpp
@@ -2,6 +2,21 @@
 
 int main()
 {
-    //
-    
+    //Declare Array
+    char message[5] {'H', 'e', 'l', 'l', 'o'};
+
+    //Print out the array through looping
+    for(auto c : message){
+        std::cout << c;
+    }
+
+    //change characters in our array
+    message[1] = 'a';
+
+    //Print out the array through looping
+    std::cout << "message : ";
+    for(auto c : message){
+        std::cout << c;
+    }
+    std::cout << std::endl;
 }
