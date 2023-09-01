02d39ab3300119c3e9093953dec7d05e1529b7a2
CodyBenn <zolnyy@yahoo.com>
2023-08-31 21:36:38 -0500

    Project 36 Pointer to Char

--- Project 36 Pointer to Char/main.cpp
+++ Project 36 Pointer to Char/main.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
-    
+    //Doesn't allow modification 
+    const char* message {"Hello World!"};
+    std::cout << "message : " << message << std::endl;
+
+    //*message = "B"; //Compiler error
+    //If users attempt to change the value it wont work
+    std::cout << "*message : " << *message << std::endl;
+
+    //Allow users to modify string
+    char message1[] {"Hello World!"};
+    message1[0] = 'B';
+    std::cout << "message1 : " << message1 << std::endl;
+
+    return 0;
 }
