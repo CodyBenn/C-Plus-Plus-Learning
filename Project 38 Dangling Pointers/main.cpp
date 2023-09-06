f72bc10f4b8c74ae795ba8808f3937f88c322e38
CodyBenn <zolnyy@yahoo.com>
2023-09-05 23:07:40 -0500

    Project 38

--- Project 38 Dangling Pointers/main.cpp
+++ Project 38 Dangling Pointers/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
+    //Case1 : Uninitialized pointer
+    int *p_number; // Dangling uninitialized pointer
 
+    std::cout << std::endl;
+    std::cout << "Case 1 : Uninitialized pointer : ." << std::endl;
+    std::cout << "p_number : " << p_number << std::endl;
+    std::cout << "*p_number : " << *p_number << std::endl;
+    
     return 0;
 }
