9e288c66c9f52d7bb3487dcec8ce431b5dd3b11e
CodyBenn <zolnyy@yahoo.com>
2023-09-07 23:57:09 -0500

    Project 39 Prep

--- Project 39 When New Fails/main.cpp
+++ Project 39 When New Fails/main.cpp
@@ -9,13 +9,19 @@ int main()
         int * data = new int[10000000000];
     }
     */
-    for(size_t i{0}; i < 100000000; ++i){
+
+    /*
+    //Exception Mechanism
+    for(size_t i{0}; i < 100; ++i){
         try{
             int * data = new int[10000000000];
         }catch(std::exception& ex){
-        std::cout << " Something went wrong : " << ex.what() << std::endl;
+        std::cout << "Something went wrong : " << ex.what() << std::endl;
         }
     }
+    */
+
+    
 
 
 
