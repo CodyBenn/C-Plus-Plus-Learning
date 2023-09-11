029d2c1ceeb344d5571a8621b3b0a6c14354a234
CodyBenn <zolnyy@yahoo.com>
2023-09-10 23:07:06 -0500

    Project 43 Declaring And Using References

--- Project 43 Declaring And Using References/main.cpp
+++ Project 43 Declaring And Using References/main.cpp
@@ -23,5 +23,29 @@ int main()
     std::cout << "ref_double_data : " << ref_double_data << std::endl;
     std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
 
+    int_data = 111;
+    double_data = 67.2;
+
+    //Print stuff out again
+    
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "Round 2 starting" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "int_data : " << int_data << std::endl;
+    std::cout << "&int_data : " << &int_data << std::endl;
+    std::cout << "double_data : " << double_data << std::endl;
+    std::cout << "&double_data : " << &double_data << std::endl;
+
+    std::cout << "================================" << std::endl;
+
+    std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+
+
+
  return 0;   
 }
