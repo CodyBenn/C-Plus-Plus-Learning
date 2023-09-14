2a519d21706a2907568e4d07a39f77bd09efa577
CodyBenn <zolnyy@yahoo.com>
2023-09-13 21:16:40 -0500

    Project 45 Referennces and Const

--- Project 45 References And Const/main.cpp
+++ Project 45 References And Const/main.cpp
@@ -2,7 +2,30 @@
 
 int main()
 {
-    
+    //Non const reference
+    std::cout << std::endl;
+    std::cout << "Non const reference  : " << std::endl;
+    int age {27};
+    int& ref_age{age};
+
+    /*
+    std::cout << "age : " << age << std::endl;
+    std::cout << "ref_age : " << ref_age << std::endl;
+
+    //Can modify original variable through reference
+
+    std::cout << std::endl;
+    std::cout << "Modify original variable through reference : " << std::endl;
+
+    ref_age++; // Modifies through reference
+
+    std::cout << "age : " << age << std::endl;
+    std::cout << "ref_age : " << ref_age << std::endl;
+    */
+
+    //Simulating reference behaviour with pointers
+    const int *const p_age{&age};
+    *p_age = 45; //Pointer will tell you that you cannot modify
 
     return 0;
 }
