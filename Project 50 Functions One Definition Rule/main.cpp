edbf1e43f0047fb1afae9ddd9e6af50e1b55cd77
CodyBenn <zolnyy@yahoo.com>
2023-09-20 21:17:50 -0500

    Project 50 prep

--- Project 50 Functions One Definition Rule/main.cpp
+++ Project 50 Functions One Definition Rule/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include "person.h"
 
-//Variable : Declation and definition
+//Variable : Declaration and definition
 double weight {};
 
 double add(double a, double b);
 
-int main()
+struct Point
 {
-    double result = add(10, 20);
-    std::cout << "result : " << result << std::endl; 
+    double m_x{};
+    double m_y{};
+};
+
+int main(){
+
+    Person p1("John Snow",35);
+    p1.print_info();
+
 
     return 0;
 }
 
+/*
 double add(double a, double b){
     return a + b;
 }
+*/
