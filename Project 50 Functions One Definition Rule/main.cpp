e2154a74903c4718860c3b685f56c011207560fc
CodyBenn <zolnyy@yahoo.com>
2023-09-20 21:07:45 -0500

    Project 50 prep

--- Project 50 Functions One Definition Rule/main.cpp
+++ Project 50 Functions One Definition Rule/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+//Variable : Declation and definition
+double weight {};
+
+double add(double a, double b);
+
 int main()
 {
-    
+    double result = add(10, 20);
+    std::cout << "result : " << result << std::endl; 
 
     return 0;
+}
+
+double add(double a, double b){
+    return a + b;
 }
